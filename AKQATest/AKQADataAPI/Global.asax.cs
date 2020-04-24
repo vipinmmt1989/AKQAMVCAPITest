e38e2097133bcc182002f7b4ade02f848597b5fd
vipin kumar <vipin_kumar20@optum.com>
2020-04-24 14:50:35 +0530

    Added Swagger

--- AKQATest/AKQADataAPI/Global.asax.cs
+++ AKQATest/AKQADataAPI/Global.asax.cs
@@ -18,6 +18,7 @@ namespace AKQADataAPI
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+           
         }
     }
 }
