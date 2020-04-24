e42ad85634ef2c2b37d8fc76a56dd7b2e9601e5f
vipin kumar <vipin_kumar20@optum.com>
2020-04-24 15:13:08 +0530

    Bit adjusted

--- AKQATest/AKQADataAPI/Controllers/ValuesController.cs
+++ AKQATest/AKQADataAPI/Controllers/ValuesController.cs
@@ -8,13 +8,14 @@ using System.Web.Http;
 
 namespace AKQADataAPI.Controllers
 {
+    [Route("api/[Controller]")]
+    
     public class ValuesController : ApiController
     {
         // GET api/values/5
         WordConverter wordConverter = new WordConverter();
        
         [HttpGet]
-        [Route("api/Values/Get")]
         public  IHttpActionResult Get(string number)
         {
             return  Ok(new { Word = wordConverter.ValidateNumber(number) });
